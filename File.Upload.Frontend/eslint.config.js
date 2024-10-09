2b97f993ebf54340ccc2cc33d5ac97d69cb0d9c5
Riri-Inferno <takayo.uenter36@icloud.com>
2024-10-10 03:28:12 +0900

    prettier入れた

--- File.Upload.Frontend/eslint.config.js
+++ File.Upload.Frontend/eslint.config.js
@@ -1,14 +1,46 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginVue from "eslint-plugin-vue";
-
+import jsPlugin from "@eslint/js";
+import vuePlugin from "eslint-plugin-vue";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import eslintConfigPrettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
-  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  ...pluginVue.configs["flat/essential"],
-  {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
+  // 対象ファイルの指定
+  {
+    files: ["**/*.{js,mjs,cjs,ts,vue}"],
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      // TypeScript パーサーの設定
+      parser: tsParser,
+      parserOptions: {
+        project: './tsconfig.json', // 必要に応じてパスを設定
+      },
+    },
+    plugins: {
+      // プラグインの指定
+      '@typescript-eslint': tsPlugin,
+      'vue': vuePlugin,
+    },
+    // ルールの設定を適用
+    rules: {
+      ...jsPlugin.configs.recommended.rules,
+      ...tsPlugin.configs.recommended.rules,
+      ...vuePlugin.configs['essential'].rules,
+    },
+  },
+   // Prettier プラグインの追加
+  {
+    plugins: {
+      'prettier': prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  eslintConfigPrettier,
 ];
